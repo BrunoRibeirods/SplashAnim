caec27a619ee3f3dc9923c26a37fea1c3ae67fef
Bruno <brunorbds50@gmail.com>
2020-11-11 11:17:25 -0300

    Dots

--- app/src/main/java/com/teste/testesplash/SplashActivity.kt
+++ app/src/main/java/com/teste/testesplash/SplashActivity.kt
@@ -7,9 +7,11 @@ import android.os.Handler
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import kotlinx.android.synthetic.main.activity_splash.*
+import kotlinx.android.synthetic.main.dot_layout.*
 
 class SplashActivity : AppCompatActivity() {
     lateinit var animation: Animation
+    lateinit var animation_dot: Animation
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
@@ -17,10 +19,13 @@ class SplashActivity : AppCompatActivity() {
         animation = AnimationUtils.loadAnimation(this, R.anim.logo_anim)
         filmly.animation = animation
 
+        animation_dot = AnimationUtils.loadAnimation(this, R.anim.logo_anim)
+        lazy_dot.animation = animation_dot
+
         Handler().postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 3000)
+        }, 5000)
     }
 }
